cf8f2e4533500159ce6c8df90ed0363a0845222f
gloglow <db_98@naver.com>
2022-10-27 13:24:53 +0900

    20221027

---
+++ Baekjoon2557.java
@@ -0,0 +1,10 @@
+package java_practice;
+
+// BAEKJOON  PROBLEM - https://www.acmicpc.net/problem/2557
+// 20221027
+
+public class Baekjoon2557 {
+	public static void main(String [] args) {
+		System.out.println("Hello World!");
+	}
+}
