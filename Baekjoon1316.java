6c0b732cd9571bef5ee1bcfec23d3d9eb2edd2bc
gloglow <db_98@naver.com>
2022-11-16 02:39:18 +0900

    20221116

---
+++ Baekjoon1316.java
@@ -0,0 +1,42 @@
+package java_practice;
+import java.io.*;
+
+public class Baekjoon1316 {
+
+	static boolean gcheck(String str) {
+		if (str.length()<3)
+			return true;
+		char tmp;
+		int pos;
+		for (int i=0; i<str.length()-1; i++) {
+			pos=i+1;
+			while(str.charAt(i)==str.charAt(pos)) {
+				pos++;
+				i++;
+				if(pos>=str.length()-1)
+					return true;
+			}
+			for (int j=pos+1; j<str.length(); j++) {
+				if (str.charAt(i)==str.charAt(j))
+					return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		int n;
+		int cnt=0;
+		n=Integer.parseInt(br.readLine());
+		for (int i=0; i<n; i++) {
+			if (gcheck(br.readLine()))
+				cnt++;
+		}
+		bw.write(""+cnt);
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
