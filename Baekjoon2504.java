e7079380d06ee7fe3a7778ae9c84bbc4b760b7a2
gloglow <db_98@naver.com>
2022-12-05 15:59:51 +0900

    20221205

--- Baekjoon2504.java
+++ Baekjoon2504.java
@@ -1,49 +1,54 @@
 //BAEKJOON  PROBLEM - https://www.acmicpc.net/problem/2504
-//20221202
+//20221205
 package java_practice;
 
-import java.io.*;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Baekjoon2504 {
-	static int cal(String str) {
-		Stack<Character> st1=new Stack<>();
-		Stack<Character> st2=new Stack<>();
-		int rst=0;
+	static int cal(Deque<Character> str1) {
+		Deque<Character>str2=new ArrayDeque<>();
 		int tmp=1;
-		for(int i=0; i<str.length(); i++)
-			st1.push(str.charAt(i));
-		st2.push(st1.pop());
-		while(!st1.empty()) {
-			if(st2.empty())
-				st2.push(st1.pop());
-			if((st2.peek()==')'&&st1.peek()=='(')
-					||(st2.peek()==']'&&st1.peek()=='[')) {
-				if(st2.peek()==')')
+		int rst=0;
+		
+		while(!str1.isEmpty()) {
+			str2.push(str1.pop());
+			while(!str1.isEmpty()&&!str2.isEmpty()&&((str1.peek()=='('&&str2.peek()==')')||
+					(str1.peek()=='['&&str2.peek()==']'))) {
+				str1.pop();
+				if(str2.peek()==')')
 					tmp*=2;
-				else
+				else if(str2.peek()==']')
 					tmp*=3;
-				st2.pop();
-				st1.pop();
-			}
-			else {
-				if(!st1.empty())
-					st2.push(st1.pop());
-			}
-			if(st2.empty()) {
-				rst+=tmp;
-				tmp=1;
+				str2.pop();
+				if(!str1.isEmpty()&&(str1.peek()==')'||str1.peek()==']'))
+					tmp=tmp+cal(str1);
 			}
 		}
+		rst=tmp;
+		while(!str2.isEmpty())
+			str1.add(str2.pop());
 		return rst;
 	}
+	
+	
 	public static void main(String[] args)throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 		String str=br.readLine();
+		Deque<Character> tmp=new ArrayDeque<>();
 		int rst=0;
 
-		rst=cal(str);
+		for (int i=0; i<str.length(); i++)
+			tmp.push(str.charAt(i));
+		rst=cal(tmp);
+		if(!tmp.isEmpty())
+			rst=0;
 		
 		bw.write(""+rst);
 		bw.flush();
