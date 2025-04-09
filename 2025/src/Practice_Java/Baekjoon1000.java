93801cfc07a4c85f9ce493d21c518d26b4e02b8f
YOONYUSANG <db_98@naver.com>
2025-04-09 22:04:14 +0900

    Baekjoon1000

---
+++ 2025/src/Practice_Java/Baekjoon1000.java
@@ -0,0 +1,24 @@
+package Practice_Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon1000 {
+
+	
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer sTokenizer = new StringTokenizer(bReader.readLine());
+		
+		int n, m;
+		
+		n = Integer.parseInt(sTokenizer.nextToken());
+		m = Integer.parseInt(sTokenizer.nextToken());
+		
+		System.out.println(n+m);
+	}
+
+}
