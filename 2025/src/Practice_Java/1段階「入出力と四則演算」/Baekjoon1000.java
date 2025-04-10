bac3d8299d816ca727a9ca01578b18d54319b36b
YOONYUSANG <db_98@naver.com>
2025-04-10 23:13:03 +0900

    Backjoon10926, 11382, 18108

--- 2025/src/Practice_Java/1段階「入出力と四則演算」/Baekjoon1000.java
+++ 2025/src/Practice_Java/1段階「入出力と四則演算」/Baekjoon1000.java
@@ -7,7 +7,6 @@ import java.util.StringTokenizer;
 
 public class Baekjoon1000 {
 
-	
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
---
+++ 2025/src/Practice_Java/1段階「入出力と四則演算」/Baekjoon10926.java
@@ -0,0 +1,18 @@
+package Practice_Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon10926 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.println(st.nextToken() + "??!");
+	}
+
+}
